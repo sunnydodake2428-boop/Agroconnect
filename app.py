0601bada5faa74f84e21ee22dac2028645a039e9
sunnydodake2428-boop <sunnydodake2428@gmail.com>
2026-03-11 17:53:15 +0530

    fix: disable debug mode for production

--- app.py
+++ app.py
@@ -58,5 +58,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=False)
